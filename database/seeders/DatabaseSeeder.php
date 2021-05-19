235e842e4c1563f78cd4d05ec8e635953c7eb450
amrgomaa <amrgumaa@gmail.com>
2021-05-19 04:24:29 +0300

    timezone added for user 1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(timezonestableSeeder::class);
+        $this->call(TimezonestableSeeder::class);
     }
 }
